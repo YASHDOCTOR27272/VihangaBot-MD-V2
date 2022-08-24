62abffc0dbaf4d04751154d81375fa1cb7f8322d
YASHDOCTOR27272 <110610538+YASHDOCTOR27272@users.noreply.github.com>
2022-08-24 14:41:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,20 +18,20 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '94781288153' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
-global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
+global.owner = ['94781288153',''] //මෙතන number එක.
+global.premium = ['94781288153','94781288153'] //මෙතන number එක.
+global.botname = 'PRAVEESH BOT DR' //මෙතන නම.
+global.ownername = 'DOCTOR MD' //මෙතන නම.
+global.ownername = 'doctor dr' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/HVXe1LAx2rmB4Bp36CP5PH' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/HVXe1LAx2rmB4Bp36CP5PH' //මෙතන grouplink එක.
+global.packname = 'PRAVEESH BOT DR' //මෙතන නම.
+global.cap = '*By PPraveesh MD*' //මෙතන නම.
+global.img = 'https://drive.google.com/file/d/12ySQA-DXjIFBNrEBuMjjNjmd2XA-TirE/view?usp=drivesdk' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.ytchannel = 'https://chat.whatsapp.com/HVXe1LAx2rmB4Bp36CP5PH' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
 global.besiAwal = '15' //මෙතන මාරු කරන්න එපා.
@@ -52,7 +52,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = 'PRAVEESH BOT DR' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
